a536d66ad782e69c89a24c3277ae354ddebeba91
João Gabriel <joaoname9@gmail.com>
2023-07-18 04:22:46 -0300

    feat: adding divImage and divCharacterStats

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,26 @@
-import Header from '@/components/Header'
+import Header from "@/components/Header";
+import Image from "next/image";
+import batman from "../../public/batman.png"
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-between w-[100%] min-h-screen p-24 sm: p-[1rem]">
-        <Header/>
-    </main>
-  )
+    <>
+      <Header />
+      <main className="flex flex-col md:flex-row items-center justify-between w-[100%] h-screen p-2 md:p-10 bg-blue-700">
+        
+        {/* DIV IMAGE */}
+        <div className="w-[100%] md:w-[40%] h-[100%] bg-green-500">
+          <Image
+            src={batman}
+            width="400"
+            height="400"
+            alt="Picture of the author"
+          />
+        </div>
+
+        {/* DIV CHARACTER STATS */}
+        <div className="w-[100%] md:w-[60%] h-[100%] bg-red-500"></div>
+      </main>
+    </>
+  );
 }
